ee6e349d6bb2883b201b1c7c3e8e9f28c5602bc5
Polina Raguilo <Polinar2007@mail.ru>
2021-02-14 16:10:18 +0300

    Added cross for hamburger menu

--- css/header/menu/menu.js
+++ css/header/menu/menu.js
@@ -1,13 +1,7 @@
 const menu = document.querySelector(".menu__list"),
-  menuItem = document.querySelectorAll(".menu_item"),
   hamburger = document.querySelector(".hamburger");
 
-hamburger.addEventListener("click", () => {;
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle('hamburger_active');
   menu.classList.toggle("menu_active");
 });
-
-// menuItem.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     menu.classList.toggle("menu_active");
-//   });
-// });
